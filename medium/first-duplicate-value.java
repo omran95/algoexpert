112bf0237fe0d4db2f36064049b3befadcf0fe66
Abdelrahman Omran <abdelrhmanalaa39@gmail.com>
2023-08-29 16:17:09 +0200

    Update first-duplicate-value.java

--- medium/first-duplicate-value.java
+++ medium/first-duplicate-value.java
@@ -1,23 +1,5 @@
 import java.util.*;
 
-class Program {
-
-  public int firstDuplicateValue(int[] array) {
-    int earliestDuplicatedIndex = Integer.MAX_VALUE;
-    for(int i = 0; i < array.length; i++) {
-      for(int j = i + 1; j < array.length; j++) {
-        if(array[j] == array[i]) {
-          earliestDuplicatedIndex = Math.min(earliestDuplicatedIndex, j);
-        }
-      }
-    }
-    return earliestDuplicatedIndex == Integer.MAX_VALUE ? -1 : array[earliestDuplicatedIndex];
-  }
-}
-
-
-
-// anotehr solution
 
 class Program {
   public int firstDuplicateValue(int[] array) {
